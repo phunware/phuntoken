992ec45ee632b411fff8047ba67433cd0ccd3ad0
Seshu <svavilikolanu@phunware.com>
2019-08-22 12:27:06 -0500

    Cleaned up import statements

--- contracts/PhunToken.sol
+++ contracts/PhunToken.sol
@@ -2,29 +2,22 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Detailed.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Mintable.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Burnable.sol";
-// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Pausable.sol";
 
-
-contract PhunToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, ERC20Pausable {
+contract PhunToken is ERC20, ERC20Detailed, ERC20Burnable, ERC20Pausable {
 
     // modify token name
-    string public constant NAME = "PhunToken";
+    string public constant _name = "Phun";
     // modify token symbol
-    string public constant SYMBOL = "PHUN";
+    string public constant _symbol = "PHUN";
     // modify token decimals
-    uint8 public constant DECIMALS = 18;
+    uint8 public constant _decimals = 18;
     // modify initial token supply
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS)); // 10000 tokens
+    uint256 public constant INITIAL_SUPPLY = 10000000000 * (10 ** uint256(_decimals)); // 10B tokens
 
-    constructor () public ERC20Detailed(NAME, SYMBOL, DECIMALS) {
+    constructor () public ERC20Detailed(_name, _symbol, _decimals) {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 
